a8099cede95bbbd946159f128b0f13f10a257955
Fabian <fabian.rupin@wheelwork.fr>
2018-08-30 10:56:31 +0200

    update to V1.07

--- Data/index.php
+++ Data/index.php
@@ -1,25 +1,63 @@
 <?php
-$target_dir = "./";
-// Get the Filename
-$source_file = urldecode($_GET["Filename"]);
-// Set the complete name of the future saved file
-$target_file = $target_dir . $source_file;
-// Get the content of the file
+// Set the destination directory
+$target_dir = "./Logs/";
+// Get the date with the wanted format
+$date = date("Ymdhis");
+// Form the new file name of the file that will be saved
+$underscore = "_";
+$extension = ".json";
+$filename = urldecode($_GET["Filename"]);
+// Get the file content from the POST
 $json_data = file_get_contents("php://input");
-if (stristr($source_file, 'asso_request_') == FALSE) {
-// If it is not an association request: save the file
+
+// Check if the file is an association request
+if( stristr($filename, "asso_request_") == FALSE )
+{
+    if( stristr($filename, "d_") != FALSE )
+    {
+        $head = "data_";
+        $devEUI = substr($filename, 2, 16);
+    }
+    else if( stristr($filename, "a_") != FALSE )
+    {
+        $head = "alarm_";
+        $devEUI = substr($filename, 2, 16);
+    }
+    else if( stristr($filename, "topology_lora") != FALSE )
+    {
+        $head = "paired_devices";
+        $devEUI = "";
+    }
+
+    $target_file = $target_dir . $head . $devEUI . $underscore . $date . $extension;
+
+    // Save this content in the new created file
     file_put_contents($target_file, $json_data);
-} else {
-// It is an association request: send an OK answer file
-// Get the devEUI
-    $devEUI = substr($source_file, 13, 16);
-// Answer
-    $devStatus = "OK";
-// Format the complete file
-    $json_answer = "{\r\n\t\"LoRa_GW_Asso_Answer_File\":
-{\r\n\t\t\"End_Device_ID\": {\r\n\t\t\t\"DevEUI\": \"$devEUI\"\r\n\t\t},\r\n\t\t\"Asso_status\": \"$devStatus\"\r\n\t}\r\n}\r\n";
-// Prepare the header
-    header('Content-Type: application/json');
-// Send the answer
-    echo $json_answer;
 }
+else
+{
+    // The file is an association request
+    // Get the end-device devEUI
+    $devEUI = substr($filename, 13, 16); // Get rid of "asso_request_" and ".json"
+
+    // OK file
+    $fileToAns = "{\n\"LoRa_GW_Asso_Answer_File\":{\n\"End_Device_ID\":{\n\"DevEUI\":\"$devEUI\",\n},\n\"Asso_status\":\"OK\"\n}\n}";
+
+    // NOK file
+    //$fileToAns = "{\n\"LoRa_GW_Asso_Answer_File\":{\n\"End_Device_ID\":{\n\"DevEUI\":\"$devEUI\",\n},\n\"Asso_status\":\"KO\"\n}\n}";
+
+    // WAIT file
+    //$fileToAns = "{\n\"LoRa_GW_Asso_Answer_File\":{\n\"End_Device_ID\":{\n\"DevEUI\":\"$devEUI\",\n},\n\"Asso_status\":\"WAIT\"\n}\n}";
+
+    header('Content-Type: application/json');
+
+    // Send the file as an answer to the POST
+    echo $fileToAns;
+
+    $head = "asso_request_";
+    $target_file = $target_dir . $head . $devEUI . $underscore . $date . $extension;
+
+    // Save this content in the new created file
+    file_put_contents($target_file, $json_data);
+}
+?>
